0dc9ff2ca47ddbc123893bd6e6fb9c4ff5afe24c
kavindiyashmi <kavindiyashmi99@gmail.com>
2024-12-17 07:55:30 +0000

    created connection file

---
+++ connection/conection.php
@@ -0,0 +1,9 @@
+<?php
+ $hostname ="127.0.0.1";
+ $username ="root";
+ $password ="mariadb";
+ $dbname ="Zero Clothing ";
+
+ $connection = mysqli_connect($hostname,$username,$password ,$dbname);
+
+ ?>
