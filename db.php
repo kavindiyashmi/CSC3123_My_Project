41dd924fa64d9c50f2b60f5e5b7264ff13cc3afc
kavindiyashmi <kavindiyashmi99@gmail.com>
2025-01-17 08:36:08 +0000

    update db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db = 'zeroy_clothing';
-$user = 'root'; // Your database username
-$pass = ''; // Your database password
+$user = 'root'; 
+$pass = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
